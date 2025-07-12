2e235bd82cb83a8a84e12feb914ee86b53b739df
Max <olejnikmax@gmail.com>
2025-07-12 16:48:29 +0300

    some things

--- src/input.c
+++ src/input.c
@@ -76,7 +76,7 @@ bool	validate_input_and_parse_map(int ac, char **av, t_scene *scene)
 	fd = open(av[1], O_RDONLY);
 	if (fd < 0)
 	{
-		ft_fprintf(2, "Error: Could not open file %s\n", av[1]);
+		ft_fprintf(2, "Error\nCould not open file %s\n", av[1]);
 		return (false);
 	}
 	read_map_into_scene(fd, scene);
