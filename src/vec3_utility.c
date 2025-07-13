f17364928216b3211378890f7d30d600a1f592ce
datagore <104622881+datagore@users.noreply.github.com>
2025-07-13 12:54:23 +0300

    Reorganize vec3 functions a little

--- src/vec3_utility.c
+++ src/vec3_utility.c
@@ -1,5 +1,12 @@
 #include "minirt.h"
 
+// Convenience function for making a 3D vector from xyz-coordinates.
+
+t_vec3	vec3(float x, float y, float z)
+{
+	return ((t_vec3){{x, y, z}});
+}
+
 // Do a linear interpolation between two 3D vectors. Returns a if t = 0, b if
 // t = 1, and a point on the line between a and b when 0 ≤ t ≤ 1.
 
@@ -17,9 +24,9 @@ t_vec3	vec3_lerp(t_vec3 a, t_vec3 b, float t)
 uint32_t	vec3_to_color(t_vec3 color)
 {
 	const float		gamma = 2.3f;
-	const uint32_t	r = (uint32_t)(powf(color.r, gamma) * 255.5f);
-	const uint32_t	g = (uint32_t)(powf(color.g, gamma) * 255.5f);
-	const uint32_t	b = (uint32_t)(powf(color.b, gamma) * 255.5f);
+	const uint32_t	r = 255.5f * powf(color.r, gamma);
+	const uint32_t	g = 255.5f * powf(color.g, gamma);
+	const uint32_t	b = 255.5f * powf(color.b, gamma);
 
 	return ((r << 24) | (g << 16) | (b << 8) | 255);
 }
