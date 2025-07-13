5cc4706617be76ebfa89568dbab41f1d7583d8d4
oliynykmax <olejnikmax@gmail.com>
2025-07-13 18:45:36 +0300

    now malloc for array

--- src/main.c
+++ src/main.c
@@ -32,7 +32,7 @@ void	parse_scene(t_scene *scene, char *filename)
 
 int	main(int ac, char **av)
 {
-	t_scene			scene;
+	t_scene	scene;
 
 	ft_bzero(&scene, sizeof(t_scene));
 	if (!validate_input_and_parse_map(ac, av, &scene))
