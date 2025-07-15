7b77c6500d64984feeed401ae214418e9b920280
Max <olejnikmax@gmail.com>
2025-07-15 16:26:16 +0300

    imter

--- src/assert.c
+++ src/assert.c
@@ -1,21 +1,23 @@
 #include "minirt.h"
 
-bool	mrt_assert(bool condition, char *msg)
+bool	mrt_assert(bool condition, char *format, ...)
 {
 	const int	errno_value = errno;
+	va_list		args;
 
 	if (condition == true)
 		return (true);
 	ft_fprintf(2, "Error\n");
-	if (msg != NULL || errno_value != 0)
+	if (format != NULL || errno_value != 0)
 	{
 		if (errno_value != 0)
 		{
 			ft_putstr_fd(strerror(errno_value), STDERR_FILENO);
 			ft_putstr_fd(": ", STDERR_FILENO);
 		}
-		ft_putstr_fd(msg, STDERR_FILENO);
-		ft_putstr_fd("\n", STDERR_FILENO);
+		va_start(args, format);
+		printf_fd(STDERR_FILENO, format, &args);
+		va_end(args);
 	}
 	return (false);
 }
--- src/input.c
+++ src/input.c
@@ -1,5 +1,46 @@
 #include "minirt.h"
 
+int	array_len(char **array)
+{
+	int	i;
+
+	if (!array)
+		return (0);
+	i = 0;
+	while (array[i])
+		i++;
+	return (i);
+}
+
+double	ft_atof(const char *str)
+{
+	double	result;
+	double	divisor;
+	int		sign;
+
+	result = 0.0;
+	divisor = 10.0;
+	sign = 1;
+	while (*str == ' ' || *str == '\t' || *str == '\n')
+		str++;
+	if (*str == '+' || *str == '-')
+		if (*str++ == '-')
+			sign = -1;
+	while (*str >= '0' && *str <= '9')
+		result = result * 10.0 + (*str++ - '0');
+	if (*str == '.')
+	{
+		str++;
+		while (*str >= '0' && *str <= '9')
+		{
+			result += (*str - '0') / divisor;
+			divisor *= 10.0;
+			str++;
+		}
+	}
+	return (result * sign);
+}
+
 void	cleanup_scene(t_scene *sc)
 {
 	if (!sc)
@@ -45,33 +86,36 @@ bool	vec3_in_range(t_vec3 v, float lower, float upper)
 			- epsilon) && (v.z <= upper + epsilon));
 }
 
-double	ft_atof(const char *str)
+/*
+ * Parses a string "x,y,z" into a vec3 and checks if values are within range
+ * @param str The string to parse in format "x,y,z"
+ * @param out Pointer to store the parsed vector
+ * @param min Minimum allowed value (ignored if min == max)
+ * @param max Maximum allowed value (ignored if min == max)
+ * @return true on success, false on failure
+ */
+bool	parse_vec3(char *str, t_vec3 *out, float min, float max)
 {
-	double	result;
-	double	divisor;
-	int		sign;
+	char	**split;
 
-	result = 0.0;
-	divisor = 10.0;
-	sign = 1;
-	while (*str == ' ' || *str == '\t' || *str == '\n')
-		str++;
-	if (*str == '+' || *str == '-')
-		if (*str++ == '-')
-			sign = -1;
-	while (*str >= '0' && *str <= '9')
-		result = result * 10.0 + (*str++ - '0');
-	if (*str == '.')
+	split = ft_split(str, ',');
+	if (!mrt_assert(split != NULL, "Memory allocation failure\n"))
 	{
-		str++;
-		while (*str >= '0' && *str <= '9')
-		{
-			result += (*str - '0') / divisor;
-			divisor *= 10.0;
-			str++;
-		}
+		free_array(split);
+		return (false);
 	}
-	return (result * sign);
+	if (!mrt_assert(array_len(split) == 3,
+			"Vector format must be 'x,y,z' with 3 values\n"))
+	{
+		free_array(split);
+		return (false);
+	}
+	*out = vec3(ft_atof(split[0]), ft_atof(split[1]), ft_atof(split[2]));
+	free_array(split);
+	if (min != max && !mrt_assert(vec3_in_range(*out, min, max),
+			"Vector values must be between %d and %d\n", (int)min, (int)max))
+		return (false);
+	return (true);
 }
 
 t_object_type	parse_type(char *s)
@@ -87,18 +131,6 @@ t_object_type	parse_type(char *s)
 	if (ft_strcmp(s, "cn") == 0)
 		return (OBJECT_CONE);
 	return (-1);
-}
-
-int	array_len(char **array)
-{
-	int	i;
-
-	if (!array)
-		return (0);
-	i = 0;
-	while (array[i])
-		i++;
-	return (i);
 }
 
 int	objects_malloc_manager(t_scene *sc)
@@ -137,23 +169,14 @@ bool	parse_shape(char **line, t_scene *sc)
 bool	parse_ambient_light(char **line, t_scene *sc)
 {
 	static bool	ambient_exists = false;
-	char		**color;
 	double		ratio;
 
 	if (ambient_exists || array_len(line) != 3)
 		return (true);
-	color = ft_split(line[2], ',');
-	if (array_len(color) != 3)
-	{
-		free_array(color);
-		return (true);
-	}
-	sc->ambient = vec3(ft_atof(color[0]), ft_atof(color[1]), ft_atof(color[2]));
-	free_array(color);
 	ratio = ft_atof(line[1]);
-	if (!mrt_assert(vec3_in_range(sc->ambient, 0, 255) && ratio >= 0.0f
-			&& ratio <= 1.0f, "Ambient: colors must be [0-255]"
-			"ratio [0.0-1.0]"))
+	if (!parse_vec3(line[2], &sc->ambient, 0, 255) || !mrt_assert(ratio >= 0.0f
+			&& ratio <= 1.0f,
+			"Ambient light ratio must be between 0.0 and 1.0\n"))
 		return (true);
 	sc->ambient = vec3_scale(vec3_scale(sc->ambient, 1.0 / 255.0), ratio);
 	ambient_exists = true;
@@ -162,8 +185,23 @@ bool	parse_ambient_light(char **line, t_scene *sc)
 
 bool	parse_point_light(char **line, t_scene *sc)
 {
-	(void)line;
-	(void)sc;
+	t_object	*obj;
+	double		brightness;
+
+	if (array_len(line) != 4 || objects_malloc_manager(sc))
+		return (true);
+	brightness = ft_atof(line[2]);
+	obj = &sc->objects[sc->object_count];
+	obj->type = OBJECT_LIGHT;
+	if (!parse_vec3(line[1], &obj->pos, 0, 0) || !parse_vec3(line[3],
+			&obj->color, 0, 255) || !mrt_assert(brightness >= 0.0f
+			&& brightness <= 1.0f,
+			"Light brightness must be between 0.0 and 1.0\n"))
+		return (true);
+	obj->color = vec3_scale(vec3_scale(obj->color, 1.0 / 255.0), brightness);
+	sc->object_count++;
+	printf("Point light: (%f, %f, %f) (%f, %f, %f)\n", obj->pos.x, obj->pos.y,
+		obj->pos.z, obj->color.x, obj->color.y, obj->color.z);
 	return (false);
 }
 
@@ -190,26 +228,15 @@ bool	parse_light(char **line, t_scene *sc)
 bool	parse_camera(char **line, t_scene *sc)
 {
 	static int	exist = 0;
-	char		**pos;
-	char		**dir;
 
 	if (exist == 1 || array_len(line) != 4)
 		return (true);
-	pos = ft_split(line[1], ',');
-	dir = ft_split(line[2], ',');
-	if (array_len(pos) != 3 || array_len(dir) != 3)
-	{
-		free_array(pos);
-		free_array(dir);
-		return (true);
-	}
-	sc->camera_pos = vec3(ft_atof(pos[0]), ft_atof(pos[1]), ft_atof(pos[2]));
-	sc->camera_dir = vec3(ft_atof(dir[0]), ft_atof(dir[1]), ft_atof(dir[2]));
 	sc->camera_fov = ft_atof(line[3]);
-	free_array(pos);
-	free_array(dir);
+	if (!parse_vec3(line[1], &sc->camera_pos, 0, 0) || !parse_vec3(line[2],
+			&sc->camera_dir, -1, 1))
+		return (true);
 	exist = 1;
-	return (!mrt_assert(vec3_in_range(sc->camera_dir, -1, 1),
+	return (!mrt_assert(fabsf(vec3_length(sc->camera_dir) - 1.0f) < 0.001f,
 			"Camera direction components must be between -1 and 1")
 		|| !mrt_assert(sc->camera_fov >= 0.0f && sc->camera_fov <= 180.0f,
 			"Camera FOV must be between 0 and 180 degrees"));
@@ -228,7 +255,6 @@ int	process_line(t_scene *sc, char *buff)
 	}
 	if (ft_strcmp(line[0], "C") == 0)
 	{
-		sc->object_count--;
 		error = parse_camera(line, sc);
 	}
 	else if (ft_strcmp(line[0], "L") == 0 || ft_strcmp(line[0], "A") == 0)
@@ -253,13 +279,12 @@ bool	read_map_into_scene(int fd, t_scene *sc)
 			break ;
 		if (!ft_str_is_whitespace(buff))
 		{
-			if (objects_malloc_manager(sc) || process_line(sc, buff))
+			if (process_line(sc, buff))
 			{
 				free(buff);
 				cleanup_scene(sc);
 				return (false);
 			}
-			sc->object_count++;
 		}
 		free(buff);
 	}
