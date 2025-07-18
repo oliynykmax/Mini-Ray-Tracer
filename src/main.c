ab0dab7f7bd5e20885abc8bc3b583e2ded541d87
datagore <104622881+datagore@users.noreply.github.com>
2025-07-18 21:52:09 +0300

    Add frame accumulation-based antialiasing

--- src/main.c
+++ src/main.c
@@ -6,15 +6,17 @@
 
 int	main(int ac, char **av)
 {
-	t_scene	scene;
+	t_scene		scene;
+	t_render	render;
 
 	ft_bzero(&scene, sizeof(t_scene));
+	ft_bzero(&render, sizeof(render));
 	if (!validate_input_and_parse_map(ac, av, &scene))
 	{
 		cleanup_scene(&scene);
 		return (1);
 	}
-	render_scene(&scene);
+	render_scene(&render, &scene);
 	cleanup_scene(&scene);
 	free(scene.objects);
 	return (0);
