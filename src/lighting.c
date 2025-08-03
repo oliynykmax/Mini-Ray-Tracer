3660f6f85f8ee378815c3f665a8ecfd3678a7cc0
datagore <104622881+datagore@users.noreply.github.com>
2025-08-03 14:09:29 +0300

    Fix norm for PBR lighting code finally

--- src/lighting.c
+++ src/lighting.c
@@ -16,11 +16,12 @@ static float	brdf_geo_dist(t_pbr *p)
 
 // Calculate the fresnel factor (F) of the Cook-Torrance BRDF.
 
-static t_vec3	brdf_fresnel(float hdotv, t_vec3 f0)
+static t_vec3	brdf_fresnel(t_pbr *p)
 {
-	const float	factor = powf(1.0f - hdotv, 5.0f);
+	const float		factor = powf(1.0f - p->hdotv, 5.0f);
+	const t_vec3	one = vec3(1.0f, 1.0f, 1.0f);
 
-	return (vec3_add(f0, vec3_scale(vec3_add(vec3(1, 1, 1), f0), factor)));
+	return (vec3_add(p->f0, vec3_scale(vec3_add(one, p->f0), factor)));
 }
 
 // Check if a light is visible from a shading point (located at `ro`). This is
@@ -47,47 +48,52 @@ static bool	light_visible(t_ray *r, t_object *light, t_vec3 ro)
 	return (true);
 }
 
-static t_vec3	one_light(t_ray *r, t_object *light, t_vec3 p, t_vec3 n, t_pbr *pbr)
+static t_vec3	one_light(t_ray *r, t_object *light, t_pbr *p)
 {
-	t_vec3	lv = vec3_sub(light->pos, p);
-	t_vec3	v = vec3_scale(r->rd, -1.0f);
-	t_vec3	l = vec3_normalize(lv);
-	t_vec3	h = vec3_normalize(vec3_add(v, l));
-	t_vec3	radiance = vec3_scale(light->color, 1.0f / vec3_dot(lv, lv));
-	pbr->ndotv = saturate(vec3_dot(n, v));
-	pbr->ndotl = saturate(vec3_dot(n, l));
-	pbr->ndoth = saturate(vec3_dot(n, h));
-	pbr->hdotv = saturate(vec3_dot(h, v));
-	t_vec3	fresnel = brdf_fresnel(pbr->hdotv, pbr->f0);
-	t_vec3	diffuse = vec3_sub(vec3(1.0f, 1.0f, 1.0f), fresnel);
-	diffuse = vec3_scale(diffuse, 1.0f - pbr->metallic);
-	t_vec3	specular = vec3_scale(fresnel, brdf_geo_dist(pbr));
-	specular = vec3_scale(specular, 1.0f / (4.0f * pbr->ndotv * pbr->ndotl + 1e-4f));
-	return vec3_scale(vec3_mul(vec3_add(vec3_scale(vec3_mul(diffuse, pbr->albedo), 1.0f / M_PI), specular), radiance), pbr->ndotl);
+	t_vec3	radiance;
+	t_vec3	spec;
+	t_vec3	diff;
+
+	p->view_dir = vec3_scale(r->rd, -1.0f);
+	p->light = vec3_sub(light->pos, p->point);
+	p->light_dir = vec3_normalize(p->light);
+	p->halfway = vec3_normalize(vec3_add(p->view_dir, p->light_dir));
+	p->ndotv = saturate(vec3_dot(p->normal, p->view_dir));
+	p->ndotl = saturate(vec3_dot(p->normal, p->light_dir));
+	p->ndoth = saturate(vec3_dot(p->normal, p->halfway));
+	p->hdotv = saturate(vec3_dot(p->halfway, p->view_dir));
+	spec = brdf_fresnel(p);
+	diff = vec3_sub(vec3(1.0f, 1.0f, 1.0f), spec);
+	diff = vec3_scale(diff, (1.0f - p->metallic) / M_PI);
+	diff = vec3_mul(diff, p->albedo);
+	spec = vec3_scale(spec, brdf_geo_dist(p));
+	spec = vec3_scale(spec, 1.0f / (4.0f * p->ndotv * p->ndotl + 1e-4f));
+	radiance = vec3_scale(light->color, 1.0f / vec3_dot(p->light, p->light));
+	return (vec3_scale(vec3_mul(vec3_add(diff, spec), radiance), p->ndotl));
 }
 
-t_vec3	apply_lighting(t_ray *r, t_object *object, t_vec3 p, t_vec3 albedo)
+t_vec3	apply_lighting(t_ray *r, t_object *object, t_vec3 point, t_vec3 albedo)
 {
-	t_pbr		pbr;
+	t_pbr		p;
 	t_vec3		color;
-	t_vec3		n;
 	t_object	*light;
 	size_t		i;
 
-	i = -1;
-	pbr.metallic = 0.9f;
-	pbr.rough = 0.1f;
-	pbr.f0 = vec3_lerp(vec3(0.04f, 0.04f, 0.04f), albedo, pbr.metallic);
-	pbr.albedo = albedo;
+	p.point = point;
+	p.albedo = albedo;
+	p.metallic = 0.9f;
+	p.rough = 0.1f;
+	p.f0 = vec3_lerp(vec3(0.04f, 0.04f, 0.04f), p.albedo, p.metallic);
+	p.normal = object_normal(object, p.point);
+	p.normal = vec3_scale(p.normal, copysignf(1, -vec3_dot(r->rd, p.normal)));
+	p.point = vec3_add(p.point, vec3_scale(p.normal, 1e-5f));
 	color = vec3_mul(albedo, r->scene->ambient);
-	n = object_normal(object, p);
-	n = vec3_scale(n, copysignf(1.0f, -vec3_dot(r->rd, n)));
-	p = vec3_add(p, vec3_scale(n, 1e-5f));
+	i = -1;
 	while (++i < r->scene->object_count)
 	{
 		light = &r->scene->objects[i];
-		if (light->type == OBJECT_LIGHT && light_visible(r, light, p))
-			color = vec3_add(color, one_light(r, light, p, n, &pbr));
+		if (light->type == OBJECT_LIGHT && light_visible(r, light, p.point))
+			color = vec3_add(color, one_light(r, light, &p));
 	}
 	return (color);
 }
