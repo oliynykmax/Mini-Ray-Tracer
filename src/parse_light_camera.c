3c13978bcc4691d3a313234240b6e1febfab689b
Max <olejnikmax@gmail.com>
2025-08-09 17:08:51 +0300

    done

--- src/parse_light_camera.c
+++ src/parse_light_camera.c
@@ -1,44 +1,41 @@
 #include "minirt.h"
 
-bool	parse_amb_light(char **line, t_scene *sc)
+void	parse_amb_light(t_parse *m)
 {
 	static bool	ambient_exists = false;
 	double		ratio;
 
-	if (ambient_exists || array_len(line) != 3)
-		return (true);
-	ratio = ft_atof(line[1]);
-	if (!parse3(line[2], &sc->ambient, 0, 255) || !mrt_assert(ratio >= 0.0f
-			&& ratio <= 1.0f,
-			"Ambient light ratio must be between 0.0 and 1.0\n"))
-		return (true);
-	sc->ambient = vec3_scale(vec3_scale(sc->ambient, 1.0 / 255.0), ratio);
+	mrt_assert(m, array_len(m->line) == 3, "Invalid ambient light format\n");
+	mrt_assert(m, !ambient_exists, "Ambient light already exists\n");
+	ratio = ft_atof(m->line[1]);
+	parse3(m, m->line[2], &m->sc->ambient, (float []){0, 255});
+	mrt_assert(m, ratio >= 0.0f && ratio <= 1.0f,
+		"Ambient light ratio must be between 0.0 and 1.0\n");
+	m->sc->ambient = vec3_scale(vec3_scale(m->sc->ambient, 1.0 / 255.0), ratio);
 	ambient_exists = true;
-	return (false);
 }
 
-bool	parse_point_light(char **line, t_scene *sc)
+void	parse_point_light(t_parse *m)
 {
-	t_object	*obj;
-	double		brightness;
+	double	brightness;
 
-	if ((array_len(line) != 4 && array_len(line) != 5)
-		|| objects_malloc_manager(sc))
-		return (true);
-	brightness = ft_atof(line[2]);
-	obj = &sc->objects[sc->object_count];
-	obj->type = OBJECT_LIGHT;
-	if (array_len(line) == 5)
-		obj->radius = ft_atof(line[4]);
-	if (!parse3(line[1], &obj->pos, 0, 0) || !parse3(line[3],
-			&obj->color, 0, 255) || !mrt_assert(brightness >= 0.0f,
-			"Light brightness must be positive\n"))
-		return (true);
+	m->arrlen = array_len(m->line);
+	mrt_assert(m, m->arrlen != 4 && m->arrlen != 5,
+		"Invalid point light format\n");
+	objects_malloc_manager(m);
+	brightness = ft_atof(m->line[2]);
+	m->obj = &m->sc->objects[m->sc->object_count];
+	m->obj->type = OBJECT_LIGHT;
+	m->obj->radius = ft_atof(m->line[4]) * m->arrlen == 5;
+	mrt_assert(m, m->obj->radius >= 0.0f, "Light radius must be positive\n");
+	parse3(m, m->line[1], &m->obj->pos, (float []){0, 0});
+	parse3(m, m->line[3], &m->obj->color, (float []){0, 255});
+	mrt_assert(m, brightness >= 0.0f, "Light brightness must be positive\n");
 	mrt_warning(brightness <= 1.0f, "Light brightness is over 1.0\n");
-	obj->color = vec3_scale(vec3_scale(obj->color, 1.0 / 255.0), brightness);
-	obj->rot = quat_from_axis_angle(vec3(0.0f, -1.0f, 0.0f), 0.0f);
-	sc->object_count++;
-	return (false);
+	m->obj->color = vec3_scale(vec3_scale(m->obj->color, 1.0 / 255.0),
+			brightness);
+	m->obj->rot = quat_from_axis_angle(vec3(0.0f, -1.0f, 0.0f), 0.0f);
+	m->sc->object_count++;
 }
 
 // Then in read_map_into_scene, when errors occur:
@@ -51,27 +48,25 @@ bool	parse_point_light(char **line, t_scene *sc)
  * do the same with fov but without spliting
  *
  */
-bool	parse_camera(char **line, t_scene *sc)
+void	parse_camera(t_parse *m)
 {
 	static bool	camera_exist = false;
 
-	if (camera_exist || (array_len(line) != 4 && array_len(line) != 6))
-		return (true);
-	if (array_len(line) == 6)
-	{
-		sc->focus_depth = ft_atof(line[4]);
-		sc->aperture_size = ft_atof(line[5]);
-	}
-	sc->fov = ft_atof(line[3]);
-	if (!parse3(line[1], &sc->pos, 0, 0) || !parse3(line[2], &sc->dir, -1, 1))
-	    return (true);
+	m->arrlen = array_len(m->line);
+	mrt_assert(m, m->arrlen >= 4 && m->arrlen <= 6, "Invalid camera format\n");
+	mrt_assert(m, !camera_exist, "Camera already exist\n");
+	m->sc->focus_depth = parse_float(m->arrlen > 4, m->line[4], 0.0f);
+	m->sc->aperture_size = parse_float(m->arrlen > 5, m->line[5], 0.0f);
+	m->sc->fov = ft_atof(m->line[3]);
+	parse3(m, m->line[1], &m->sc->pos, (float []){0, 0});
+	parse3(m, m->line[2], &m->sc->dir, (float []){-1, 1});
 	camera_exist = true;
-	return (!mrt_assert(fabsf(vec3_length(sc->dir) - 1.0f) < 0.001f,
-			"Camera direction components must be between -1 and 1\n")
-		|| !mrt_assert(sc->fov >= 0.0f && sc->fov <= 180.0f,
-			"Camera FOV must be between 0 and 180 degrees\n")
-		|| !mrt_assert(sc->focus_depth >= 0.0f,
-			"Camera focus depth must be non-negative\n")
-		|| !mrt_assert(sc->aperture_size >= 0.0f,
-			"Camera aperture size must be non-negative\n"));
+	mrt_assert(m, fabsf(vec3_length(m->sc->dir) - 1.0f) < 0.001f,
+		"Camera direction components must be between -1 and 1\n");
+	mrt_assert(m, m->sc->fov >= 0.0f && m->sc->fov <= 180.0f,
+		"Camera FOV must be between 0 and 180 degrees\n");
+	mrt_assert(m, m->sc->focus_depth >= 0.0f,
+		"Camera focus depth must be non-negative\n");
+	mrt_assert(m, m->sc->aperture_size >= 0.0f,
+		"Camera aperture size must be non-negative\n");
 }
