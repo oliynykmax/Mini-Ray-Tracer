0d26d030ad1e4f2fc179f0d4a40a75a781e6076c
Max <olejnikmax@gmail.com>
2025-08-10 10:27:16 +0300

    invalid read size fixed

--- src/parse_utils.c
+++ src/parse_utils.c
@@ -1,29 +1,27 @@
 #include "minirt.h"
 
-t_texture	parse_texture(bool exists, t_parse *map, const char *token)
+t_texture	parse_texture(bool exists, t_parse *map, int i)
 {
-	if (!exists)
+	if (!exists || map->line[i] == NULL || ft_strcmp(map->line[i], "_") == 0)
 		return (TEXTURE_NONE);
-	if (ft_strcmp(token, "checker") == 0)
+	if (ft_strcmp(map->line[i], "checker") == 0)
 		return (TEXTURE_CHECKED);
-	if (ft_strcmp(token, "zigzag") == 0)
+	if (ft_strcmp(map->line[i], "zigzag") == 0)
 		return (TEXTURE_ZIGZAG);
-	if (ft_strcmp(token, "polkadot") == 0)
+	if (ft_strcmp(map->line[i], "polkadot") == 0)
 		return (TEXTURE_POLKADOT);
-	if (ft_strcmp(token, "bump") == 0)
+	if (ft_strcmp(map->line[i], "bump") == 0)
 		return (TEXTURE_BUMP);
-	if (ft_strcmp(token, "_") == 0)
-		return (TEXTURE_NONE);
 	mrt_assert(map,
 		false,
 		"Unknown texture '%s' (expected checker | zigzag | polkadot | bump)\n",
-		token);
+		map->line[i]);
 	return (TEXTURE_NONE);
 }
 
-float	parse_float(bool exists, char *str, float std)
+float	parse_float(bool exists, t_parse *map, float std, int i)
 {
-	if (!exists || str == NULL || ft_strcmp(str, "_") == 0)
+	if (!exists || map->line[i] == NULL || ft_strcmp(map->line[i], "_") == 0)
 		return (std);
-	return (ft_atof(str));
+	return (ft_atof(map->line[i]));
 }
