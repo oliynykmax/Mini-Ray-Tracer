b93e37733ec5c9ae971b403bb24dd9a75e1defd4
Max <olejnikmax@gmail.com>
2025-08-12 23:49:36 +0300

    parse optionals finished

--- src/parse_utils.c
+++ src/parse_utils.c
@@ -10,8 +10,6 @@ t_texture	parse_texture(bool exists, t_parse *map, int i)
 		return (TEXTURE_ZIGZAG);
 	if (ft_strcmp(map->line[i], "polkadot") == 0)
 		return (TEXTURE_POLKADOT);
-	if (ft_strcmp(map->line[i], "bump") == 0)
-		return (TEXTURE_BUMP);
 	fatal_if(map,
 		true,
 		"Unknown texture '%s' (expected checker | zigzag | polkadot | bump)\n",
@@ -26,18 +24,15 @@ float	parse_float(bool exists, t_parse *map, float std, int i)
 	return (ft_atof(map->line[i]));
 }
 
-void	parse_optionals(t_parse *m, int t_index)
+void	parse_optionals(t_parse *m, int i)
 {
-	const int	rough_index = t_index + 1;
-	const int	metal_index = t_index + 2;
-
-	m->obj->texture = parse_texture(m->arrlen > t_index, m, t_index);
-	m->obj->rough = parse_float(m->arrlen > rough_index, m, DEFAULT_ROUGH,
-			rough_index);
-	fatal_if(m, m->obj->rough < 0.0f || m->obj->rough > 1.0f,
+	m->obj->rough = parse_float(m->arrlen > i, m, DEFAULT_ROUGH, i);
+	fatal_if(m, m->obj->rough < 0.01f || m->obj->rough > 1.0f,
 		"Rough must be in [0,1]\n");
-	m->obj->metallic = parse_float(m->arrlen > metal_index, m,
-			DEFAULT_METALLIC, metal_index);
+	m->obj->metallic = parse_float(m->arrlen > i + 1, m,
+			DEFAULT_METALLIC, i + 1);
 	fatal_if(m, m->obj->metallic < 0.0f || m->obj->metallic > 1.0f,
 		"Metallic must be in [0,1]\n");
+	m->obj->texture = parse_texture(m->arrlen > i + 2, m, i + 2);
+	m->obj->bump = parse_texture(m->arrlen > i + 3, m, i + 3);
 }
