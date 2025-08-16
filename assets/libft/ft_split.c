cb45a4234a5be4159217daec3a11b952a8c9cccc
oliynykmax <olejnikmax@gmail.com>
2025-08-16 15:50:27 +0300

    funcheck fix

--- assets/libft/ft_split.c
+++ assets/libft/ft_split.c
@@ -6,68 +6,60 @@
 /*   By: maoliiny <maoliiny@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 15:04:34 by maoliiny          #+#    #+#             */
-/*   Updated: 2025/05/18 22:20:42 by maoliiny         ###   ########.fr       */
+/*   Updated: 2025/08/16 17:20:00 by maoliiny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_strchar(char c, char str)
+static size_t	split(const char *string, char delimiter, char **array)
 {
-	if (str == c)
-		return (1);
-	return (0);
-}
+	size_t	array_length;
+	size_t	start;
+	size_t	index;
 
-static char	*ft_strdup_loc(const char *src, int n)
-{
-	char	*dest;
-	int		i;
-
-	i = 0;
-	dest = (char *)malloc((n + 1));
-	while (i < n && src[i])
+	index = 0;
+	array_length = 0;
+	while (string[index] != '\0')
 	{
-		dest[i] = src[i];
-		i++;
+		while (string[index] == delimiter)
+			index++;
+		start = index;
+		while (string[index] != delimiter && string[index] != '\0')
+			index++;
+		if (start != index)
+		{
+			if (array != NULL)
+			{
+				array[array_length] = ft_substr(string, start, index - start);
+				if (array[array_length] == NULL)
+					return (-1);
+			}
+			array_length++;
+		}
 	}
-	dest[i] = '\0';
-	return (dest);
+	return (array_length);
 }
 
-static int	ft_sp(const char *str, char charset, char **tab)
+char	**ft_split(const char *string, char delimiter)
 {
-	int	start;
-	int	counter;
-	int	i;
+	size_t	array_length;
+	char	**array;
 
-	counter = 0;
-	i = 0;
-	while (str[i])
-	{
-		while (str[i] && ft_strchar(str[i], charset))
-			i++;
-		start = i;
-		while (!ft_strchar(str[i], charset) && str[i])
-			i++;
-		if (tab && start < i)
-			tab[counter] = ft_strdup_loc(&str[start], i - start);
-		if (start < i)
-			counter++;
-	}
-	return (counter);
-}
-
-char	**ft_split(char const *s, char c)
-{
-	char	**tab;
-	int		i;
-
-	i = ft_sp(s, c, NULL);
-	tab = malloc(sizeof(char *) * (i + 1));
-	if (!tab)
+	if (string == NULL)
 		return (NULL);
-	ft_sp(s, c, tab);
-	tab[i] = 0;
-	return (tab);
+	array_length = split(string, delimiter, NULL);
+	array = ft_calloc((array_length + 1), sizeof(char *));
+	if (array != NULL)
+	{
+		array[array_length] = NULL;
+		if (split(string, delimiter, array) == (size_t) - 1)
+		{
+			while (array_length-- != 0)
+				free(array[array_length]);
+			free(array);
+			return (NULL);
+		}
+	}
+	return (array);
 }
