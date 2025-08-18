d8bde565e14f46d46ec68ebf32e5c3993f7771a4
oliynykmax <olejnikmax@gmail.com>
2025-08-18 18:40:46 +0300

    simple main

--- src/main.c
+++ src/main.c
@@ -11,23 +11,16 @@ static void	free_scene_objects(t_scene *scene)
 		++i;
 	}
 	free(scene->objects);
-	scene->objects = NULL;
-	scene->object_count = 0;
 }
 
 static void	init_structs(t_scene *scene, t_render *render, t_parse *map)
 {
-	ft_bzero(scene, sizeof(t_scene));
-	ft_bzero(render, sizeof(t_render));
-	ft_bzero(map, sizeof(t_parse));
+	*scene = (t_scene){0};
+	*render = (t_render){0};
+	*map = (t_parse){0};
 	map->sc = scene;
 	map->fd = -1;
-	map->ambient_exists = false;
-	map->camera_exists = false;
-	map->temp_split = NULL;
 	render->scene = scene;
-	scene->objects = NULL;
-	map->num_of_lights = 0;
 }
 
 int	main(int ac, char **av)
